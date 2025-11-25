4da4de04c374643bee6936707497c17025af5142
Hawaz <hawazt@gmail.com>
2025-11-25 16:23:03 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/carryconnect/api/CarryConnectApisApplication.java
@@ -0,0 +1,13 @@
+package com.carryconnect.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryConnectApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryConnectApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carryconnect/api/CarryConnectApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carryconnect.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryConnectApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
